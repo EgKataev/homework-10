21e72d57ec7f66a4b7ee5b04b8c9908a70ab0b57
EgKataev <113854561+EgKataev@users.noreply.github.com>
2022-11-01 16:41:14 +0300

    Начало!!!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
 public class Main {
+    public static void printIsLeapYear(int Yer) {
+        int year = 2022;
+        boolean yearIsLeap = isLeap(year);
+        printIsLeapYearResult(year, yearIsLeap);
+    }
+
+    private static boolean isLeap(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    private static void printIsLeapYearResult(int year, boolean yearIsLeap) {
+        if (yearIsLeap) {
+            System.out.println(year + " високосный");
+        } else {
+            System.out.println(year + " не является високосным");
+        }
+    }
+
+    public static void recommendApplicationVersion(int clientOS, int deviceYear) {
+        boolean deviceIsOld = isDeviceOld(deviceYear);
+        System.out.print("Установите ");
+        if (deviceIsOld) {
+            System.out.print("lite ");
+        }
+        System.out.print("версию для ");
+        if (clientOS == 0) {
+            System.out.println("iOS");
+        } else {
+            System.out.println("Android");
+        }
+    }
+
+    private static boolean isDeviceOld(int deviceYear) {
+        return false;
+    }
+
+
+    public static void printSeparator() {
+        System.out.println("+++++++");
+        System.out.println("-------");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Задание 1");
+        System.out.println("Лекция с Ильёй");
+        int[] issuesByMonths = {4, 6, 7, 9, 2, 5, 12, 3, 7, 10, 6, 7, 1, 8};
+        printSeparator();
+        for (int i = 0; i < issuesByMonths.length; i++) {
+            System.out.println(issuesByMonths[i]);
+            if ((i + 1) % 3 == 0) {
+                printSeparator();
+            }
+        }
+        printSeparator();
+
+        public static void calculateDeliveryDays ( int deliveryDistance){
+            System.out.println("Задание 3");
+            int deliveryDays = 1;
+            if (deliveryDistance > 20) {
+                deliveryDays++;
+            }
+            if (deliveryDistance > 60) {
+                deliveryDays++;
+            }
+            System.out.println("Потребуется дней: " + deliveryDays);
+        }
     }
 }
